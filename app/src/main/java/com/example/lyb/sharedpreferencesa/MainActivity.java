e895d46662d04c16b10032db10ccb75c4b018f00
Tony <tony@gmail.com>
2017-04-10 16:43:19 +0800

    向SharedPreferences.Editor对象添加数据

--- app/src/main/java/com/example/lyb/sharedpreferencesa/MainActivity.java
+++ app/src/main/java/com/example/lyb/sharedpreferencesa/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.lyb.sharedpreferencesa;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import es.dmoral.toasty.Toasty;
@@ -14,14 +17,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toasty.error(MainActivity.this,"你是谁？", Toast.LENGTH_SHORT,true).show();
-        Toasty.success(MainActivity.this, "Success!", Toast.LENGTH_SHORT, true).show();
-        Toasty.info(MainActivity.this, "Here is some info for you.", Toast.LENGTH_SHORT, true).show();
-        Toasty.warning(MainActivity.this, "Beware of the dog.", Toast.LENGTH_SHORT, true).show();
-        Toasty.normal(MainActivity.this, "Normal toast w/o icon").show();
-
-        //Toasty.normal(MainActivity.this, "Normal toast w/ icon", ic_launcher).show();
-
-
+        Button saveData = (Button) findViewById(R.id.save_data);
+        saveData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                editor.putString("name","lyb");
+                editor.putInt("age",23);
+                editor.apply();
+            }
+        });
     }
 }
