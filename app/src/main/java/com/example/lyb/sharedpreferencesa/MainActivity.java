c89756cbad8351355e64438af245f27c61fa153a
Tony <tony@gmail.com>
2017-04-11 14:15:20 +0800

    取数据打印log

--- app/src/main/java/com/example/lyb/sharedpreferencesa/MainActivity.java
+++ app/src/main/java/com/example/lyb/sharedpreferencesa/MainActivity.java
@@ -3,13 +3,9 @@ package com.example.lyb.sharedpreferencesa;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
-
-import es.dmoral.toasty.Toasty;
-
-import static com.example.lyb.sharedpreferencesa.R.mipmap.ic_launcher;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,10 +27,13 @@ public class MainActivity extends AppCompatActivity {
         restoreData.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                SharedPreferences.Editor editor = getSharedPreferences("data",MODE_PRIVATE).edit();
+                SharedPreferences pref = getSharedPreferences("data",MODE_PRIVATE);
+                String name = pref.getString("name","");
+                int age = pref.getInt("age",0);
+                Log.d("MainActivity","name is "+name);
+                Log.d("MainActivity","age is "+age);
             }
         });
     }
-
 }
 
